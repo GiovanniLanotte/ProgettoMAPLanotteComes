a6c219a18e2fc5262264ccb70bea7da2cb2b0e74
Giovanni <Giovanni@giovanni-pc.lan>
2017-05-10 19:29:02 +0200

    [Prima esercitazione MAP] inseriti commenti nella classe Attribute

--- src/Attribute.java
+++ src/Attribute.java
@@ -1,9 +1,13 @@
-
+/*Definiamo la classe astratta Attribute che modella un generico attributo discreto o continuo.*/
 public class Attribute {
-private String name;
-private int index;
+private String name;  //nome simbolico dell'attributo
+private int index;  //identificativo numerico dell'attributo
 
-public Attribute(String name, int index){
+//name e index sono private perchè variabili dell'oggetto
+
+public Attribute(String name, int index){ 
+	//Questo metodo inizializza i valori dei membri name e index
+	//assume come input valori per nome simbolico e identificativo numerico dell'attributo
 	this.name=name;
 	this.index=index;
 }
