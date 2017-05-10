f670cc247035e93f35e466845cebb951d3f6fcd5
Giovanni <Giovanni@giovanni-pc.lan>
2017-05-10 19:30:16 +0200

    [Prima Esercitazione MAP] aggiunta del metodo getNumberOfDistinctValues()

--- src/DiscreteAttribute.java
+++ src/DiscreteAttribute.java
@@ -7,6 +7,11 @@ public DiscreteAttribute(String name, int index, String values[]){
 	super(name,index);
 	this.values=values;//passo un riferimento
 }
+
+public int getNumberOfDistinctValues(){
+	return values.length;
+}
+
 public String getValue(int index){
 	return values[index];
 }
