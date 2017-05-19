f4d8cf9194b7dbf5255705cd2e1b16248143e4b5
catia <catia@172.26.25.128>
2017-05-19 12:36:54 +0200

    [Quarta esercitazione MAP]-Modifica del costruttore della classe Data tale che la Temperatura sia continua e assuma valori numerici nell’intervallo [0, 30,3] ed inserimento dei valori nell'oggetto data

--- src/data/Data.java
+++ src/data/Data.java
@@ -41,11 +41,7 @@ public class Data {
 		attributeSet.add(new DiscreteAttribute("Outlook",0, outLookValues));
 		
 		//Avvaloriamo gli attributi di Temperature
-		String temperatureValue[] = new String[3];
-		temperatureValue[0]="hot";
-		temperatureValue[1]="mild";
-		temperatureValue[2]="cool";
-		attributeSet.add(new DiscreteAttribute("temperature", 1, temperatureValue));
+		attributeSet.add(new ContinuousAttribute("temperature", 1, 0.0f,30.3f));
 		
 		//Avvaloro gli attibuti di humidity
 		String humidityValue[] = new String[2];
@@ -69,85 +65,85 @@ public class Data {
 
 		// TO DO : memorizzare le transazioni secondo lo schema della tabella nelle specifiche
 		data[0][0]=outLookValues[2];
-		data[0][1]=temperatureValue[0];
+		data[0][1]=new Float(30.0);
 		data[0][2]=humidityValue[0];
 		data[0][3]=windValue[0];
 		data[0][4]=playTennisValue[0];
 		
 		data[1][0]=outLookValues[2];
-		data[1][1]=temperatureValue[0];
+		data[1][1]=new Float(30.3);
 		data[1][2]=humidityValue[0];
 		data[1][3]=windValue[1];
 		data[1][4]=playTennisValue[0];
 
 		data[2][0]=outLookValues[0];
-		data[2][1]=temperatureValue[0];
+		data[2][1]=new Float(30);
 		data[2][2]=humidityValue[0];
 		data[2][3]=windValue[0];
 		data[2][4]=playTennisValue[1];
 
 		data[3][0]=outLookValues[1];
-		data[3][1]=temperatureValue[1];
+		data[3][1]=new Float(13);
 		data[3][2]=humidityValue[0];
 		data[3][3]=windValue[0];
 		data[3][4]=playTennisValue[1];
 
 		data[4][0]=outLookValues[1];
-		data[4][1]=temperatureValue[2];
+		data[4][1]=new Float(0);
 		data[4][2]=humidityValue[1];
 		data[4][3]=windValue[0];
 		data[4][4]=playTennisValue[1];
 
 		data[5][0]=outLookValues[1];
-		data[5][1]=temperatureValue[2];
+		data[5][1]=new Float(0);
 		data[5][2]=humidityValue[1];
 		data[5][3]=windValue[1];
 		data[5][4]=playTennisValue[0];
 
 		data[6][0]=outLookValues[0];
-		data[6][1]=temperatureValue[2];
+		data[6][1]=new Float(0.1);
 		data[6][2]=humidityValue[1];
 		data[6][3]=windValue[1];
 		data[6][4]=playTennisValue[1];
 
 		data[7][0]=outLookValues[2];
-		data[7][1]=temperatureValue[1];
+		data[7][1]=new Float(13);
 		data[7][2]=humidityValue[0];
 		data[7][3]=windValue[0];
 		data[7][4]=playTennisValue[0];
 
 		data[8][0]=outLookValues[2];
-		data[8][1]=temperatureValue[2];
+		data[8][1]=new Float(0.1);
 		data[8][2]=humidityValue[1];
 		data[8][3]=windValue[0];
 		data[8][4]=playTennisValue[1];
 
 		data[9][0]=outLookValues[1];
-		data[9][1]=temperatureValue[1];
+		data[9][1]=new Float(12);
 		data[9][2]=humidityValue[1];
 		data[9][3]=windValue[0];
 		data[9][4]=playTennisValue[1];
 
 		data[10][0]=outLookValues[2];
-		data[10][1]=temperatureValue[1];
+		data[10][1]=new Float(12.5);
 		data[10][2]=humidityValue[1];
 		data[10][3]=windValue[1];
 		data[10][4]=playTennisValue[1];
 
 		data[11][0]=outLookValues[0];
-		data[11][1]=temperatureValue[1];
+		data[11][1]=new Float(12.5);
 		data[11][2]=humidityValue[0];
 		data[11][3]=windValue[1];
 		data[11][4]=playTennisValue[1];
 
 		data[12][0]=outLookValues[0];
-		data[12][1]=temperatureValue[0];
+		data[12][1]=new Float(29.21);
 		data[12][2]=humidityValue[1];
 		data[12][3]=windValue[0];
 		data[12][4]=playTennisValue[1];
 
 		data[13][0]=outLookValues[1];
-		data[13][1]=temperatureValue[1];
+		data[13][1]=new Float(12.5);
 		data[13][2]=humidityValue[0];
 		data[13][3]=windValue[1];
 		data[13][4]=playTennisValue[0];
