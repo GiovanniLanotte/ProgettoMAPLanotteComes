0b4afe86e94bafe532859eab398a59cfed404b66
Giovanni <Giovanni@172.26.53.213>
2017-05-19 12:58:25 +0200

    [Quarta esercitazione MAP] Creazione della classe Interval

---
+++ src/mining/Interval.java
@@ -0,0 +1,33 @@
+package mining;
+
+public class Interval {
+private float inf; //estremo inferiore
+private float sup; //estremo superiore
+
+public Interval(float inf, float sup) {
+	this.inf=inf;
+	this.sup=sup;
+}
+
+public void setInf(float inf) {
+	this.inf = inf;
+}
+
+public void setSup(float sup) {
+	this.sup = sup;
+}
+public float getInf() {
+	return inf;
+}
+public float getSup() {
+	return sup;
+}
+public boolean checkValueInclusion(float value){
+	return inf>=value && value<sup;
+}
+
+@Override
+	public String toString() {
+		return "["+inf+","+sup+"[";
+	}
+}
